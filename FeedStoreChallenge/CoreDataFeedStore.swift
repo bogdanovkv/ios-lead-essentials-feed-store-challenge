bb69964cd2f658ac42c2e6bc316f5c360574ae77
Konstatin Bogdanov <>
2021-07-09 18:16:22 +0300

    test_retrieve_hasNoSideEffectsOnNonEmptyCache passed

--- FeedStoreChallenge/CoreDataFeedStore.swift
+++ FeedStoreChallenge/CoreDataFeedStore.swift
@@ -7,19 +7,19 @@ import CoreData
 public final class CoreDataFeedStore: FeedStore {
 	private static let modelName = "FeedStore"
 	private static let model = NSManagedObjectModel(name: modelName, in: Bundle(for: CoreDataFeedStore.self))
-	
+
 	private let container: NSPersistentContainer
 	private let context: NSManagedObjectContext
-	
+
 	struct ModelNotFound: Error {
 		let modelName: String
 	}
-	
+
 	public init(storeURL: URL) throws {
 		guard let model = CoreDataFeedStore.model else {
 			throw ModelNotFound(modelName: CoreDataFeedStore.modelName)
 		}
-		
+
 		container = try NSPersistentContainer.load(
 			name: CoreDataFeedStore.modelName,
 			model: model,
@@ -27,7 +27,7 @@ public final class CoreDataFeedStore: FeedStore {
 		)
 		context = container.newBackgroundContext()
 	}
-	
+
 	public func retrieve(completion: @escaping RetrievalCompletion) {
 		context.perform {
 			let request = NSFetchRequest<MOFeedImage>(entityName: "MOFeedImage")
@@ -43,17 +43,17 @@ public final class CoreDataFeedStore: FeedStore {
 			}
 		}
 	}
-	
+
 	public func insert(_ feed: [LocalFeedImage], timestamp: Date, completion: @escaping InsertionCompletion) {
 		let currentContext = context
 		currentContext.perform {
 			feed.forEach { image in
 				let _ = MOFeedImage.create(contex: self.context,
-										   timeStamp: timestamp,
-										   id: image.id,
-										   description: image.description,
-										   location: image.location,
-										   url: image.url)
+				                           timeStamp: timestamp,
+				                           id: image.id,
+				                           description: image.description,
+				                           location: image.location,
+				                           url: image.url)
 			}
 			do {
 				try currentContext.save()
@@ -63,7 +63,7 @@ public final class CoreDataFeedStore: FeedStore {
 			}
 		}
 	}
-	
+
 	public func deleteCachedFeed(completion: @escaping DeletionCompletion) {
 		fatalError("Must be implemented")
 	}
